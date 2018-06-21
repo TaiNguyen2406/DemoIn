83fb8cdad2bcea30fe40dabf2401eae683976bd0
underwaterhp93 <underwaterhp93@gmail.com>
2018-06-21 17:53:27 +0700

    Add project files.

---
+++ DemoIn/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace DemoIn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudTu = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudDen = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pbQRcode = new System.Windows.Forms.PictureBox();
+            this.lblSTT = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQRcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMa
+            // 
+            this.txtMa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMa.Location = new System.Drawing.Point(84, 12);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(245, 26);
+            this.txtMa.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã";
+            // 
+            // nudTu
+            // 
+            this.nudTu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudTu.Location = new System.Drawing.Point(84, 44);
+            this.nudTu.Maximum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.nudTu.Name = "nudTu";
+            this.nudTu.Size = new System.Drawing.Size(100, 26);
+            this.nudTu.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "STT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(190, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "--->";
+            // 
+            // nudDen
+            // 
+            this.nudDen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudDen.Location = new System.Drawing.Point(229, 44);
+            this.nudDen.Maximum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.nudDen.Name = "nudDen";
+            this.nudDen.Size = new System.Drawing.Size(100, 26);
+            this.nudDen.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(31, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(298, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "IN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pbQRcode
+            // 
+            this.pbQRcode.BackColor = System.Drawing.Color.White;
+            this.pbQRcode.Location = new System.Drawing.Point(75, 116);
+            this.pbQRcode.Name = "pbQRcode";
+            this.pbQRcode.Size = new System.Drawing.Size(200, 200);
+            this.pbQRcode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbQRcode.TabIndex = 7;
+            this.pbQRcode.TabStop = false;
+            // 
+            // lblSTT
+            // 
+            this.lblSTT.AutoSize = true;
+            this.lblSTT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSTT.Location = new System.Drawing.Point(12, 116);
+            this.lblSTT.Name = "lblSTT";
+            this.lblSTT.Size = new System.Drawing.Size(29, 15);
+            this.lblSTT.TabIndex = 8;
+            this.lblSTT.Text = "STT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 322);
+            this.Controls.Add(this.lblSTT);
+            this.Controls.Add(this.pbQRcode);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nudDen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudTu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudTu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQRcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudTu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudDen;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pbQRcode;
+        private System.Windows.Forms.Label lblSTT;
+    }
+}
+
---
+++ DemoIn/Form1.cs
@@ -0,0 +1,68 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoIn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i= Convert.ToInt32(nudTu .Value); i<=nudDen.Value;i++)
+            {
+               
+                pbQRcode.Image = null;
+                pbQRcode.Update();
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                string str = txtMa.Text.ToUpper() + String.Format("{0:D6}", i);
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(str, QRCodeGenerator.ECCLevel.Q);
+                lblSTT.Text = i.ToString();
+                QRCode qrCode = new QRCode(qrCodeData);
+                Bitmap qrCodeImage = qrCode.GetGraphic(20);
+                pbQRcode.Image = qrCodeImage;
+                PrintDocument p = new PrintDocument();
+                //PrintDialog dlgSettings = new PrintDialog();      //for choosing printer
+                //PrintPreviewDialog preview = new PrintPreviewDialog();
+                //preview.Document = p;
+                //dlgSettings.Document = p;
+                p.PrinterSettings.DefaultPageSettings.Landscape = true;
+                p.PrinterSettings.DefaultPageSettings.PaperSize = new PaperSize("Tai", 115, 45);
+                p.PrinterSettings.DefaultPageSettings.Margins = new Margins(0, 0, 0, 0);
+                p.DefaultPageSettings.PaperSize = new PaperSize("Tai", 115, 45);
+                p.DefaultPageSettings.Landscape = true;
+                p.DefaultPageSettings.Margins = new Margins(0, 0, 0, 0);
+                p.DocumentName = txtMa.Text + i.ToString();
+                p.PrintPage += delegate (object sender1, PrintPageEventArgs e1)
+                {
+                    e1.Graphics.DrawImage(qrCodeImage,10,5,35,35);
+                    e1.Graphics.DrawString(txtMa.Text.ToUpper() + "\n" + String.Format("{0:D6}", i), new Font("Arial", 9), new SolidBrush(Color.Black), new PointF(55, 8));
+                };
+                p.Print();
+
+                Application.DoEvents();
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nudDen.Maximum = int.MaxValue;
+            nudTu.Maximum = int.MaxValue;
+        }
+    }
+}
